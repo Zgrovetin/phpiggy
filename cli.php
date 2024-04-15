b24b398534a5668f7b18a413431a67fc2cb3362a
Zgrovetin <60929172+Zgrovetin@users.noreply.github.com>
2024-04-15 11:47:33 +0200

    MySQL
    204. Introduction to SQL
    205. Creating a database
    206. Creating tables
    207. Inserting data
    208. Reading data
    209. Updating data
    210. Deleting data
    211. Using PHPMyAdmin
    212. Enabling PDO extension
    213. Custom Composer scripts
    214. Understanding DSN
    215. Creating a DSN string
    216. Connecting a database
    217. The PDOException class
    218. Refactoring the database
    219. Quering the database
    220. Fetch modes
    221. SQL injections
    222.Prepared statements
    223. Understanding transactions
    224. Creating transactions with PDO
    225. Understanding data modeling
    226. Designing a user table
    227. Creating a table in an SQL file
    228. Loading files
    229. Conditionally creating tables
    230. Refactoring the query

---
+++ cli.php
@@ -0,0 +1,41 @@
+<?php
+
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database('mysql', [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpiggy'
+], 'root', '');
+
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
+
+
+
+// try {
+//     $db->connection->beginTransaction();
+
+//     $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
+
+//     $search = "Hats";
+//     $query = "SELECT * FROM products WHERE name=:name";
+
+//     $stmt = $db->connection->prepare($query);
+
+//     $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
+
+//     $stmt->execute();
+
+//     var_dump($stmt->fetchALL(PDO::FETCH_OBJ));
+
+//     $db->connection->commit();
+// } catch (Exception $error) {
+//     if ($db->connection->inTransaction()) {
+//         $db->connection->rollBack();
+//     }
+//     echo "Transaction failed!";
+// }
