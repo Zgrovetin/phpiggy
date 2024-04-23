c559ec4dc58d02a0e931b8129b7622f90a5229ff
Zgrovetin <60929172+Zgrovetin@users.noreply.github.com>
2024-04-23 19:16:11 +0200

    17. Handling file uploads
    285.Preparing the receipt controller
    286.Encoding file data
    287.Exercise:creating a receipt service
    288.Validating file upload
    289.Validating file sizes
    290.Validating filenames
    291.Validating file MIME types
    292.Generating a random filename
    293.Moving uploaded files
    294.Designing a receipts table
    295.Storing the receipt
    296.Displaying receipts
    297.Validating the download request
    298.Downloading files
    299.Deleting a receipt

--- cli.php
+++ cli.php
@@ -2,53 +2,22 @@
 
 include __DIR__ . '/src/Framework/Database.php';
 
-require __DIR__ . "/vendor/autoload.php"; //exercise1
+require __DIR__ . "/vendor/autoload.php";
 
 use Framework\Database;
-use Dotenv\Dotenv;      //exercise2
-use App\Config\Paths;   //exercise5
+use Dotenv\Dotenv;
+use App\Config\Paths;
 
-$dotenv = Dotenv::createImmutable(Paths::ROOT); //exercise3
-$dotenv->load();                                //exercise3
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
-// $db = new Database('mysql', [ //before exercise4
-//     'host' => 'localhost',          //before exercise4
-//     'port' => 3306,          //before exercise4
-//     'dbname' => 'phpiggy'         //before exercise4
-// ], 'root', '');   //before exercise4
 
-$db = new Database($_ENV['DB_DRIVER'], [ //exercise4
-    'host' => $_ENV['DB_HOST'],          //exercise4
-    'port' => $_ENV['DB_PORT'],          //exercise4
-    'dbname' => $_ENV['DB_NAME']         //exercise4
-], $_ENV['DB_USER'], $_ENV['DB_PASS']);   //exercise4
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents("./database.sql");
 
 $db->query($sqlFile);
-
-
-
-// try {
-//     $db->connection->beginTransaction();
-
-//     $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
-
-//     $search = "Hats";
-//     $query = "SELECT * FROM products WHERE name=:name";
-
-//     $stmt = $db->connection->prepare($query);
-
-//     $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
-
-//     $stmt->execute();
-
-//     var_dump($stmt->fetchALL(PDO::FETCH_OBJ));
-
-//     $db->connection->commit();
-// } catch (Exception $error) {
-//     if ($db->connection->inTransaction()) {
-//         $db->connection->rollBack();
-//     }
-//     echo "Transaction failed!";
-// }
